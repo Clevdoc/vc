bd3a5ebaffc8e1dcd3ae93d16ad60d799e359da3
Sarthak <sarthak.clevdoc@gmail.com>
2025-02-03 14:59:37 +0530

    ss

--- server/src/index.js
+++ server/src/index.js
@@ -20,7 +20,11 @@ let socketToRoom = {}; // socketID -> { roomID, username }
 const pc_config = {
   iceServers: [
     {
-      urls: "stun:13.235.24.137:3478"
+      urls: [
+        "stun:stun.l.google.com:19302",
+        "stun:stun1.l.google.com:19302",
+        "stun:stun2.l.google.com:19302"
+      ]
     }
   ]
 };
